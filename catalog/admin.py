51de962ccd3e8eefedb52efecad63703c3c170e0
maurosluciosprojetos <maurosluciosprojetos@gmail.com>
2024-10-03 09:11:51 -0300

    app para catálogo de biblioteca

---
+++ catalog/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from catalog.models import Author, Genre, Book, BookInstance
+
+admin.site.register(Genre)
+# Register your models here.
+# admin.site.register(Book)
+# admin.site.register(Author)
+# admin.site.register(BookInstance)
+
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAmdin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BookInstance)
+class bookInstncaeadmin(admin.ModelAdmin):
+    pass
